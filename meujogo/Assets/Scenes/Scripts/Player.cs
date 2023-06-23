284b6173d7bf90ca31b310a7179b541184a1b6a0
abiancamayara <bianca.mayara@escolar.ifrn.edu.br>
2023-06-23 15:41:24 -0300

    commit

---
+++ meujogo/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    
+    private bool isJumping;
+    private bool doubleJump;
+    private bool isFire;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+        Jump();
+        BowFire();
+    }
+
+    void Mover()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+
+        if (movement > 0)
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 1);
+            }
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if (movement < 0)
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 1);
+            }
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (movement == 0 && !isJumping && !isFire)
+        {
+            anim.SetInteger("transition", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 2);
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                isJumping = true;
+            }
+            else
+            {
+                if (doubleJump)
+                {
+                    anim.SetInteger("transition", 2);
+                    rig.AddForce(new Vector2(0, jumpForce * 2), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+        }
+        
+
+    }
+
+    void BowFire()
+    {
+        StartCoroutine("Fire");
+    }
+
+    IEnumerator Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isFire = true;
+            anim.SetInteger("transition", 3);
+            yield return new WaitForSeconds(0.28f);
+            anim.SetInteger("transition", 0);
+            isFire = false;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+}
